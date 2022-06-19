665cb79312999c362abc80ded9e7c48cb8e211b5
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2022-06-19 16:41:25 -0500

    threejs

--- src/components/Threejs.jsx
+++ src/components/Threejs.jsx
@@ -3,15 +3,18 @@ import { AppThree } from '../threejs';
 
 export const Threejs = () => {
   const container = useRef(null)
+  const requestId = useRef(null)
   useEffect(() => {
     const app = new AppThree(container.current)
     app.controlls();
     app.camaraSceene(0.0999, -0.0833, 0.0025);
     const cube = app.createCube()
     app.addScene(cube)
-    const animate =  app.animate()
-    const id = requestAnimationFrame(animate);
-    console.log(id)
+    function render() {
+      app.renderScene();
+      requestId.current = requestAnimationFrame(render);
+    }
+
   }, [])
   
   return (
